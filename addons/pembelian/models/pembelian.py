212c3d720b18e8c5cb411b20f8fa20ddb4075958
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-09-25 19:51:02 +0700

    feat: brands

--- addons/pembelian/models/pembelian.py
+++ addons/pembelian/models/pembelian.py
@@ -7,6 +7,7 @@ class Pembelian(models.Model):
     tanggal = fields.Date(string='Tanggal')
     status = fields.Selection([('draft', 'Draft'), ('to_approve', 'To Approve'), ('approved', 'Approved'), ('done', 'Done')], default='draft', string='Status')
     pembelian_ids = fields.One2many('pembelian.line', 'pembelian_id', string='Pembelian Ids')
+    brand_ids = fields.Many2many('pembelian.brand', 'pembelian_brand_rel', 'pembelian_id', 'brand_id', string='Brand Ids')
 
 class pembelian_line(models.Model):
     _name = 'pembelian.line'
@@ -15,3 +16,8 @@ class pembelian_line(models.Model):
     product_id = fields.Many2one('product.product', string='Product')
     quantity = fields.Float(string='Quantity', default=0.0)
     uom_id = fields.Many2one('uom.uom', string='Uom Id')
+
+class Brand(models.Model):
+    _name = 'pembelian.brand'
+
+    name = fields.Char(string='Name')
