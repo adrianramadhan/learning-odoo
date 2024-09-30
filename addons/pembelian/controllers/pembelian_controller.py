d93c52691cac432a479c1950ac488b60947f5659
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-09-30 07:09:49 +0700

    refactor: response json

--- addons/pembelian/controllers/pembelian_controller.py
+++ addons/pembelian/controllers/pembelian_controller.py
@@ -1,47 +1,55 @@
 import json
-import werkzeug.wrappers
-
-from odoo import http, _
+from odoo import http
 from odoo.http import request
 
 class PembelianRestApi(http.Controller):
     @http.route(['/api/pembelian_get'], type='http', auth='public', methods=['GET'], csrf=False)
     def pembelian_restapi_get(self, **params):
         pembelian = request.env['pembelian.pembelian'].sudo().search([])
-        dict_pembelian = {}
         data_pembelian = []
+
         for h in pembelian:
-            dict_brand = {}
             detail_brand = []
-            dict_detail_product = {}
             detail_product = []
+
             for b in h.brand_ids:
-                dict_brand = {'id': b.id, 'name': b.name}
-                detail_brand.append(dict_brand)
+                detail_brand.append({'id': b.id, 'name': b.name})
+
             for p in h.pembelian_ids:
-                dict_detail_product = {'product_id': p.product_id.display_name, 'description': p.description, 'quantity': p.quantity, 'uom_id': p.uom_id.name, 'price': p.price, 'sub_total': p.sub_total}
-                detail_product.append(dict_detail_product)
-            dict_pembelian = {'id': h.id, 'name': h.name, 'brand_ids': detail_brand, 'pembelian_ids': detail_product}
+                detail_product.append({
+                    'product_id': p.product_id.display_name,
+                    'description': p.description,
+                    'quantity': p.quantity,
+                    'uom_id': p.uom_id.name,
+                    'price': p.price,
+                    'sub_total': p.sub_total,
+                })
+
+            dict_pembelian = {
+                'id': h.id,
+                'name': h.name,
+                'brand_ids': detail_brand,
+                'pembelian_ids': detail_product,
+            }
             data_pembelian.append(dict_pembelian)
+
         data = {
             'status': 200,
             'message': 'Success',
             'data': data_pembelian
         }
+
         try:
-            return werkzeug.wrappers.Response(
-                status=200, 
-                content_type='application/json; charset=utf-8',
-                headers=[('Access-Control-Allow-Origin', '*')],  # Tambahkan izin CORS di sini
-                response=json.dumps(data)
+            return request.make_response(
+                json.dumps(data),
+                headers=[('Content-Type', 'application/json; charset=utf-8'), ('Access-Control-Allow-Origin', '*')],
             )
-        except:
-            return werkzeug.wrappers.Response(
-                status=400, 
-                content_type='application/json; charset=utf-8',
-                headers=[('Access-Control-Allow-Origin', '*')],
-                response=json.dumps({
-                    'error': 'Error', 
-                    'error_description': 'Error Description',
-                    })
+        except Exception as e:
+            return request.make_response(
+                json.dumps({
+                    'error': 'Error',
+                    'error_description': str(e),
+                }),
+                status=400,
+                headers=[('Content-Type', 'application/json; charset=utf-8'), ('Access-Control-Allow-Origin', '*')],
             )
